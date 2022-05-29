c7f61582b0de77c5b1cfb5958d8e8268e007a35a
Mateusz Mesek <mateusz@mesek.pl>
2022-05-30 00:03:40 +0200

    add support magento 2.4.4

--- src/Plugin/InjectViews/OnViewConfigReader.php
+++ src/Plugin/InjectViews/OnViewConfigReader.php
@@ -32,11 +32,13 @@ class OnViewConfigReader
                 'view_id' => $viewId,
                 'action_class' => $this->config->getMviewAction($documentName),
                 'group' => 'indexer',
+                'walker' => 'Magento\Framework\Mview\View\ChangeLogBatchWalker',
                 'subscriptions' => [
                     $subscriptionTable => [
                         'name' => $subscriptionTable,
                         'column' => 'id',
-                        'subscription_model' => null
+                        'subscription_model' => null,
+                        'processor' => 'Magento\Framework\Mview\View\AdditionalColumnsProcessor\DefaultProcessor'
                     ]
                 ],
                 'sync_document_name' => $documentName
